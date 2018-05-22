1b9c7e1e4140c5e1bad0b8b9091387154d63d12a
Wijiyanti Nur Oktawia <39021816+jiowiji@users.noreply.github.com>
2018-05-22 22:51:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,147 @@
-<?php
-header('location:halaman.html?hal=home')
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog WAT</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/blog-post.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">WAT</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <!-- Post Content Column -->
+        <div class="col-lg-8">
+
+          <!-- Title -->
+          <h1 class="mt-4">Yogyakarta</h1>
+
+          <!-- Author -->
+          <p class="lead">
+            by
+            <a href="#">WAT</a>
+          </p>
+
+          <hr>
+
+          <!-- Date/Time -->
+          <p>Dibuat pada 22 Mei 2018 at 16:40 PM</p>
+
+          <hr>
+
+          <!-- Preview Image -->
+          <img class="img-fluid rounded" src="titik-nol-km-jogja-1.jpg" alt="">
+
+          <hr>
+
+          <!-- Post Content -->
+          <p class="lead"> Daerah Istimewa Yogyakarta atau biasa disingkat dengan DIY adalah salah satu daerah otonom setingkat provinsi yang ada di Indonesia. Propinsi ini beribukota di Yogyakarta.</p> 
+
+          <p> Dari nama daerah ini yaitu Daerah Istimewa Yogyakarta sekaligus statusnya sebagai Daerah Istimewa. Status sebagai Daerah Istimewa berkenaan dengan runutan sejarah berdirinya propinsi ini, baik sebelum maupun sesudah Proklamasi Kemerdekaan Republik Indonesia.</p>
+
+          <p>Sebutan kota kebudayaan untuk kota ini berkaitan erat dengan peninggalan-peninggalan budaya bernilai tinggi semasa kerajaan-kerajaan tersebut yang sampai kini masih tetap lestari. Sebutan ini juga berkaitan dengan banyaknya pusat-pusat seni dan budaya. Sebutan kata Mataram yang banyak digunakan sekarang ini, tidak lain adalah sebuah kebanggaan atas kejayaan Kerajaan Mataram.</p>
+
+          <p>Predikat sebagai kota pelajar berkaitan dengan sejarah dan peran kota ini dalam dunia pendidikan di Indonesia. Di samping adanya berbagai pendidikan di setiap jenjang pendidikan tersedia di propinsi ini, di Yogyakarta terdapat banyak mahasiswa dan pelajar dari seluruh daerah di Indonesia. Tidak berlebihan bila Yogyakarta disebut sebagai miniatur Indonesia.</p>
+
+          <p>Disamping predikat-predikat di atas, sejarah dan status Yogyakarta merupakan hal menarik untuk disimak. Nama daerahnya memakai sebutan DIY sekaligus statusnya sebagai Daerah Istimewa. Status Yogyakarta sebagai Daerah Istimewa berkenaan dengan runutan sejarah Yogyakarta, baik sebelum maupun sesudah Proklamasi Kemerdekaan Republik Indonesia.</p>
+
+          <blockquote class="blockquote">
+            <p class="mb-0">Sejarah Singkat Provinsi Daerah Istimewa Yogyakarta</p>
+            <footer class="blockquote-footer">Daerah Yang Sangat Istimewa
+              <cite title="Source Title">Yogyakarta</cite>
+            </footer>
+          </blockquote>
+
+          <hr> 
+
+          <!-- Comments Form -->
+          <div class="card my-4">
+            <h5 class="card-header">Leave a Comment:</h5>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <textarea class="form-control" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-4">
+
+          <!-- Sidebar -->
+          <div class="card my-4">
+            <h5 class="card-header">Anggota Kelompok</h5>
+            <div class="card-body">
+              <div>
+                <ul>
+                  <li>Andhika Wahyu Arista 5150411286</li>
+                  <li>Wijiyanti Nur Oktawia 5150411297</li>
+                  <li>Amalia Harumningtyas 5150411310</li>
+                </ul>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright  Kelompok 9 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
